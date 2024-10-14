b4e5e9b6a48631ed10521099d0c20fc10f3874f4
Caio Rossi <caio_rossi56@hotmail.com>
2024-10-14 08:51:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,12 +35,6 @@ const colaboradores = [
     instagram: "https://instagram.com/theoggallo",
     whatsapp: "https://wa.me/53984735604",
   },
-  {
-    foto: "https://images.unsplash.com/photo-1576158113928-4c240eaaf360?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    nome: "Nome Sobrenome",
-    instagram: "https://instagram.com/nome",
-    whatsapp: "https://wa.me/5544332211",
-  },
 ];
 
 function displayColaboradores() {
